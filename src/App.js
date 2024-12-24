c17136c8772de023a230083c338c36d21184de16
jinyichao.321 <jinyichao.321@bytedance.com>
2024-12-24 17:37:30 +0800

    feat: implement a tree

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="xmasTree">
+      <FontAwesomeIcon icon={faStar} className="star" />
+      <div className="blessing animate__animated animate__bounceIn">
+        Merry Christmas!
+      </div>
     </div>
   );
 }
